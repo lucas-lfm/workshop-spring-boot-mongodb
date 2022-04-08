bfd9fd9d52561fdfd0e9aefdc854c650ea66eed6
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-04-07 22:02:14 -0300

    Consulta simples com @Query

--- src/main/java/com/springudemy/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/springudemy/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.springudemy.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,5 +14,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Transactional(readOnly=true)
 	public List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	public List<Post> searchTitle(String text);
 	
 }
