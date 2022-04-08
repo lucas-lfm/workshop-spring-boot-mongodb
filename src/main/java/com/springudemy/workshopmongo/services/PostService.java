9ff8b0e0bbe6e1478dfe35dab57198b2a8167b4d
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-04-07 21:09:14 -0300

    Obtendo Post por id

---
+++ src/main/java/com/springudemy/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.springudemy.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springudemy.workshopmongo.domain.Post;
+import com.springudemy.workshopmongo.repositories.PostRepository;
+import com.springudemy.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+				", Tipo: " + Post.class.getName()));
+	}
+
+}
