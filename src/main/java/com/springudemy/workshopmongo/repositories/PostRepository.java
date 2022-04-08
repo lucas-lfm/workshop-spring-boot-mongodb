5c737dbbd4358059171cfa73de92288a207fd849
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-04-07 21:53:23 -0300

    Consulta simples com query methods

--- src/main/java/com/springudemy/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/springudemy/workshopmongo/repositories/PostRepository.java
@@ -1,11 +1,17 @@
 package com.springudemy.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springudemy.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Transactional(readOnly=true)
+	public List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
